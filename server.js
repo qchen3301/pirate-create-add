e003f9575cebdac92bb2c65090dc6788a551a2e3
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-09-12 14:33:37 -0400

    server intial setup

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-//your code here
+const express = require('express')
+const app = express()
+const hbs = require('hbs')
+
+const PORT = process.env.PORT || 3000
+
+app.set('view engine', 'hbs')
+
+app.listen(PORT, function(){
+    console.log('+======================+')
+    console.log(`+LISTENING ON PORT ${PORT}+`)
+    console.log('+======================+')
+})  
+
+
